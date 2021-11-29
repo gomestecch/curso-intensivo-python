369bfff9471a8eac3d120efabbd17b3f40860587
Jackson Gomes <jacksonnanuque@hotmail.com>
2021-11-28 23:55:38 -0300

    Strings, concatenando e remvoendo espaços em branco

---
+++ livro-curso-intensivo-cap02.py
@@ -0,0 +1,30 @@
+#Strings
+
+#Mudando para Letras maiúculas e minúsculas
+name = "jackson gomes"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#Combinando ou Concatenando strings
+first_name = "Luiza"
+last_name = "lovelasce"
+full_name = first_name + " " + last_name
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+print("Mãe: Marly", "\nPai: José Mário")
+
+#Removendo espaços em branco
+favorite_language = "python "
+print(favorite_language)
+print(favorite_language.rstrip()) #removendo espaços em branco lado direito
+favorite_language = favorite_language.rstrip() #armazenando na variável
+
+favorite_language = " python"
+print(favorite_language)
+print(favorite_language.lstrip()) #removendo espaços em branco lado esquerdo
+favorite_language = favorite_language.rstrip() #armazenando na variável
+
+favorite_language = " python "
+print(favorite_language.rstrip().lstrip()) #removendo espaços em branco em ambos os lados
+favorite_language = (favorite_language.rstrip().lstrip())
