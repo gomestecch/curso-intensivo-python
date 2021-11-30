a6f1b99b2d120d430913bffd9fa21cd5600e3a2b
Jackson Gomes <jacksonnanuque@hotmail.com>
2021-11-29 23:05:13 -0300

    Configurando Extensões

--- livro-curso-intensivo-cap02.py
+++ livro-curso-intensivo-cap02.py
@@ -1,20 +1,20 @@
 #Strings
 
-#Mudando para Letras maiúculas e minúsculas
+##Mudando para Letras maiúsculas e minúsculas
 name = "jackson gomes"
 print(name.title())
 print(name.upper())
 print(name.lower())
 
-#Combinando ou Concatenando strings
+##Combinando ou Concatenando strings
 first_name = "Luiza"
-last_name = "lovelasce"
+last_name = "love"
 full_name = first_name + " " + last_name
 print(full_name)
 print("Hello, " + full_name.title() + "!")
 print("Mãe: Marly", "\nPai: José Mário")
 
-#Removendo espaços em branco
+##Removendo espaços em branco
 favorite_language = "python "
 print(favorite_language)
 print(favorite_language.rstrip()) #removendo espaços em branco lado direito
